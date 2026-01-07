fd9c2e061e093d48d504f1ade8e5218570888f88
Atul Prakash <berealatul@gmail.com>
2026-01-07 19:43:52 +0530

    Testing play pause

---
+++ play_pause_probe.lua
@@ -0,0 +1,59 @@
+state = {
+    server_ip = "10.160.189.122"
+}
+
+function descriptor()
+    return {
+        title = "Play/Pause Sync",
+        version = "0.1",
+        author = "Test",
+        shortdesc = "Manual Sync Test",
+        capabilities = {"menu"}
+    }
+end
+
+function menu()
+    return {
+        "Send PLAY",
+        "Send PAUSE",
+        "Apply REMOTE"
+    }
+end
+
+function trigger_menu(id)
+    if id == 1 then
+        send_event("play")
+    elseif id == 2 then
+        send_event("pause")
+    elseif id == 3 then
+        poll_remote()
+    end
+end
+
+function send_event(action)
+    vlc.msg.info("SENDING " .. action)
+
+    vlc.net.http.request({
+        url = "http://" .. state.server_ip .. ":8080/event",
+        method = "POST",
+        headers = { "Content-Type: application/json" },
+        data = '{"action":"' .. action .. '"}'
+    })
+end
+
+function poll_remote()
+    local response = vlc.net.http.request({
+        url = "http://" .. state.server_ip .. ":8080/poll",
+        method = "GET"
+    })
+
+    if response == "play" then
+        vlc.msg.info("APPLYING REMOTE PLAY")
+        vlc.playlist.play()
+    elseif response == "pause" then
+        vlc.msg.info("APPLYING REMOTE PAUSE")
+        vlc.playlist.pause()
+    else
+        vlc.msg.info("NO REMOTE ACTION")
+    end
+end
